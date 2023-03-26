57944867a5ac3388438f4a64d4ab2271d1ffdbd5
TetianaKabanova <110104725+TetianaKabanova@users.noreply.github.com>
2023-03-26 19:21:58 +0300

    task 01 done

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const liItem = document.querySelectorAll(".item");
+console.log(`Number of categories: ${liItem.length}`);
+
+liItem.forEach((el) => {
+  console.log(`Category: ${el.firstElementChild.textContent}`);
+  console.log(`Elements: ${el.lastElementChild.children.length}`);
+});
--- src/js/task-01.js
+++
@@ -1,6 +0,0 @@
-const liItem = document.querySelectorAll(".item");
-
-liItem.forEach((el) => {
-  console.log(`Number of categories: ${el.firstElementChild.textContent}`);
-  console.log(el.lastElementChild.children.length);
-});
