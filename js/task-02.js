114a1691bfafd6085bab07198730ddbc2a44865e
TetianaKabanova <110104725+TetianaKabanova@users.noreply.github.com>
2023-03-26 19:55:32 +0300

    task 02 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("ul#ingredients");
+
+for (const ingredient of ingredients) {
+  const liEl = document.createElement("li");
+  liEl.textContent = ingredient;
+  liEl.classList.add("item");
+  listEl.appendChild(liEl);
+}
+
+document.body.appendChild(listEl);
