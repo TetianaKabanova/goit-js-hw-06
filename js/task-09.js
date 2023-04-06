bce6fe0e1af911049987d3cadcc930733c0b39f8
TetianaKabanova <110104725+TetianaKabanova@users.noreply.github.com>
2023-04-06 11:06:25 +0200

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
+const btnChangeColor = document.querySelector(".change-color");
+const body = document.body;
+const span = document.querySelector(".color");
+btnChangeColor.addEventListener("click", onBtnClick);
+function onBtnClick() {
+  span.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
